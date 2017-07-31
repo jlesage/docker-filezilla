e9ddb256f9d1fea3a93cac1806476f18ee051f8e
Jocelyn Le Sage <jocelyn@le-sage.com>
2017-07-31 11:43:38 -0400

    Make sure $XDG_CONFIG_HOME/putty is created.  It is used to store host keys.

--- rootfs/etc/cont-init.d/filezilla.sh
+++ rootfs/etc/cont-init.d/filezilla.sh
@@ -7,4 +7,10 @@ set -u # Treat unset variables as an error.
 echo "Generating machine-id..."
 cat /proc/sys/kernel/random/uuid | tr -d '-' > /etc/machine-id
 
+# The following directory is needed to store host keys.
+mkdir -p "$XDG_CONFIG_HOME"/putty
+
+# Take ownership of the config directory.
+chown -R $USER_ID:$GROUP_ID /config
+
 # vim: set ft=sh :
