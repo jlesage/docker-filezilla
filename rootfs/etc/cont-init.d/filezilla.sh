1713b0e52f4f296473f3c7b7a951bc3289a2c666
Jocelyn Le Sage <jocelyn@le-sage.com>
2020-04-25 20:22:37 -0400

    Added the ability to install extra packages to the container image.

--- rootfs/etc/cont-init.d/filezilla.sh
+++ rootfs/etc/cont-init.d/filezilla.sh
@@ -3,14 +3,64 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
+run() {
+    j=1
+    while eval "\${pipestatus_$j+:} false"; do
+        unset pipestatus_$j
+        j=$(($j+1))
+    done
+    j=1 com= k=1 l=
+    for a; do
+        if [ "x$a" = 'x|' ]; then
+            com="$com { $l "'3>&-
+                        echo "pipestatus_'$j'=$?" >&3
+                      } 4>&- |'
+            j=$(($j+1)) l=
+        else
+            l="$l \"\$$k\""
+        fi
+        k=$(($k+1))
+    done
+    com="$com $l"' 3>&- >&4 4>&-
+               echo "pipestatus_'$j'=$?"'
+    exec 4>&1
+    eval "$(exec 3>&1; eval "$com")"
+    exec 4>&-
+    j=1
+    while eval "\${pipestatus_$j+:} false"; do
+        eval "[ \$pipestatus_$j -eq 0 ]" || return 1
+        j=$(($j+1))
+    done
+    return 0
+}
+
 log() {
-    echo "[cont-init.d] $(basename $0): $*"
+    if [ -n "${1-}" ]; then
+        echo "[cont-init.d] $(basename $0): $*"
+    else
+        while read OUTPUT; do
+            echo "[cont-init.d] $(basename $0): $OUTPUT"
+        done
+    fi
 }
 
 # Generate machine id.
 if [ ! -f /etc/machine-id ]; then
     log "generating machine-id..."
     cat /proc/sys/kernel/random/uuid | tr -d '-' > /etc/machine-id
+fi
+
+# Install requested packages.
+if [ "${INSTALL_EXTRA_PKGS:-UNSET}" != "UNSET" ]; then
+    log "installing requested package(s)..."
+    for PKG in $INSTALL_EXTRA_PKGS; do
+        if cat /etc/apk/world | grep -wq "$PKG"; then
+            log "package '$PKG' already installed"
+        else
+            log "installing '$PKG'..."
+            run add-pkg "$PKG" 2>&1 \| log
+        fi
+    done
 fi
 
 # Make sure required directories exist.
