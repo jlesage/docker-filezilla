b20ecd3d6856ad75f225f9cfe0841dd38f05edeb
Jocelyn Le Sage <jocelyn@le-sage.com>
2025-06-20 21:05:16 -0400

    Set the GDK_GL variable to prevent error message in log.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+
+# Added to avoid the following error message:
+#   MESA-LOADER: failed to open swrast: Error loading shared library
+#   /usr/lib/xorg/modules/dri/swrast_dri.so: No such file or directory
+#   (search paths /usr/lib/xorg/modules/dri, suffix _dri)
+# We could instead install `mesa-dri-gallium`, but this increases the image
+# size a lot.
+export GDK_GL=disable
+
 exec /usr/bin/filezilla --local=/storage
 # vim:ft=sh:ts=4:sw=4:et:sts=4
